053ff79b3adc69f1a457c7e0a003248c03a3015d
mcavady <james@responsivedeveloper.com>
2015-01-21 14:15:42 +0000

    note added to footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <footer><!--html5 footer -->
       <?php wp_footer(); ?>
     <div class="copyright">
-      <a href="www.responsivedeveloper.com" title="copyright"> Sharp Theme by &copy; www.responsivedeveloper.com 2015</a>
+      <a href="www.responsivedeveloper.com" title="copyright"> Sharp Theme by &copy; www.responsivedeveloper.com 2015</a><!-- logo to go here -->
     </div>
     </footer><!--html5 footer end-->
   </div><!--end the footer-->
