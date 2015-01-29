ed9b2c588f215a0e867e995c7f7aa6b197d412b6
root <root@responsivedeveloper.com>
2015-01-29 11:27:53 +0000

    Footer fix - reduced font size and layout of copyright notice

--- footer.php
+++ footer.php
@@ -1,24 +1,14 @@
-<?php
-/**
- * The Template for footer.
- *
- * @package WordPress
- * @subpackage sharp
- * @since sharp 1.0
- */
-?>
-
 </div><!--end the container -->
 <!-- footer -->
-  <div id="footer" role="contentinfo"><?php show_social_widgets(); ?>
+  <div id="footer" role="contentinfo">
     <footer><!--html5 footer -->
       <?php wp_footer(); ?>
     <div class="copyright">
 	<p><a href="www.responsivedeveloper.com" title="copyright">Copyright © www.responsivedeveloper.com</a>
 	<a href="www.responsivedeveloper.com" title="copyright">
-		<p> Theme by www.responsivedeveloper.com 2015</p>
-	</a>
+		Theme by www.responsivedeveloper.com 2015
 	<img width="25" height="25" alt="Responsive Developer" src="http://preview.responsivedeveloper.com/astrovine/wp-content/uploads/2015/01/rd-logo-wheel.png">
+	</a></p>
     </div>
     </footer><!--html5 footer end-->
   </div><!--end the footer-->
